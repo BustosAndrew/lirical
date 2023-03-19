647b154b6e70af2e5adb54a829a5f360a702d3d2
Andrew Bustos <infernious@live.com>
2023-03-19 01:34:56 -0700

    Removed axios

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
 import Head from "next/head"
-import Image from "next/image"
-import { Inter } from "next/font/google"
 import styles from "@/styles/Home.module.css"
-import axios from "axios"
 import { useEffect, useRef, useState } from "react"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
 	const [file, setFile] = useState()
