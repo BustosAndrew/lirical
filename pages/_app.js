4241e0090c6fa1e01c55d7d27ddf8b576eae874a
Andrew Bustos <infernious@live.com>
2023-03-19 04:53:29 -0700

    Added steps progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,40 @@
 import "@/styles/globals.css"
 import { ChakraProvider } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react"
+import { StepsTheme as Steps } from "chakra-ui-steps"
+
+const colors = {
+	brand: {
+		900: "#fac22c",
+		800: "#f1ecaf",
+	},
+	gray: {
+		200: "#fac22c",
+	},
+}
+
+const CustomSteps = {
+	...Steps,
+	baseStyle: (props) => {
+		return {
+			...Steps.baseStyle(props),
+			stepIconContainer: {
+				//...Steps.baseStyle(props).stepIconContainer,
+			},
+		}
+	},
+}
+
+const theme = extendTheme({
+	colors,
+	components: {
+		Steps: CustomSteps,
+	},
+})
 
 export default function App({ Component, pageProps }) {
 	return (
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<Component {...pageProps} />
 		</ChakraProvider>
 	)
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
 import Head from "next/head"
 import styles from "@/styles/Home.module.css"
 import { useEffect, useRef, useState } from "react"
+import {
+	Button,
+	Input,
+	Heading,
+	VStack,
+	Flex,
+	Center,
+	background,
+} from "@chakra-ui/react"
+import { Step, Steps, useSteps } from "chakra-ui-steps"
+
+const steps = [
+	{ label: "Step 1", content: { text: "one" } },
+	{ label: "Step 2", content: { text: "two" } },
+	{ label: "Step 3", content: { text: "three" } },
+]
 
 export default function Home() {
 	const [file, setFile] = useState()
+	const { nextStep, prevStep, setStep, reset, activeStep } = useSteps({
+		initialStep: 0,
+	})
 
 	const submitHandler = async (event) => {
 		event.preventDefault()
@@ -36,21 +55,86 @@ export default function Home() {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main className={styles.main}>
-				<div className={styles.center}>
-					<form encType='multipart/form-data' onSubmit={submitHandler}>
-						<label htmlFor='fileupload'>Upload Audio file</label>
-						<input
-							required
-							type='file'
-							name='file'
-							accept='audio/*'
-							onChange={changeFile}
-						/>
-						<button type='submit'>Submit</button>
-					</form>
-				</div>
-			</main>
+			<Flex justifyContent='center' alignContent='center' height='100vh'>
+				<Center>
+					<Flex flexDir='column' width='100%'>
+						<Steps activeStep={activeStep}>
+							{steps.map(({ label, content }) => (
+								<Step key={label}>
+									{/* {content.text} */}
+									<form encType='multipart/form-data' onSubmit={submitHandler}>
+										<VStack gap={2}>
+											<Heading color='brand.800'>Upload Audio</Heading>
+											<Input
+												required
+												type='file'
+												name='file'
+												accept='audio/*'
+												size='md'
+												color='brand.800'
+												onChange={changeFile}
+												borderColor='brand.800'
+												className={styles.upload}
+											/>
+											<Button
+												_hover={{ background: "#f1ecaf" }}
+												type='submit'
+												bgColor='brand.900'
+											>
+												Submit
+											</Button>
+										</VStack>
+									</form>
+								</Step>
+							))}
+						</Steps>
+						{activeStep === steps.length ? (
+							<Flex p={4}>
+								<Button
+									bg='brand.900'
+									mx='auto'
+									size='sm'
+									onClick={reset}
+									_hover={{ background: "#f1ecaf" }}
+								>
+									Reset
+								</Button>
+							</Flex>
+						) : (
+							<Flex width='100%' justify='flex-end'>
+								<Button
+									isDisabled={activeStep === 0}
+									mr={4}
+									onClick={prevStep}
+									size='sm'
+									variant='ghost'
+									_disabled={{
+										cursor: "not-allowed",
+										opacity: 0.2,
+									}}
+									_hover={
+										activeStep !== 0 && {
+											background: "brand.900",
+											color: "black",
+										}
+									}
+									color='brand.800'
+								>
+									Prev
+								</Button>
+								<Button
+									size='sm'
+									bg='brand.900'
+									onClick={nextStep}
+									_hover={{ background: "#f1ecaf" }}
+								>
+									{activeStep === steps.length - 1 ? "Finish" : "Next"}
+								</Button>
+							</Flex>
+						)}
+					</Flex>
+				</Center>
+			</Flex>
 		</>
 	)
 }
