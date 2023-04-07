8287eed04b9f76a38dc223480214f318f3e41ae7
Andrew Bustos <infernious@live.com>
2023-04-07 01:35:58 -0700

    Added timeout to whisper endpoint

--- pages/api/whisper.js
+++ pages/api/whisper.js
@@ -3,6 +3,7 @@ const fs = require("fs")
 const FormData = require("form-data")
 const formidable = require("formidable")
 import fetch from "node-fetch"
+// import { clearTimeout } from "timers"
 
 export const config = {
 	api: {
@@ -33,6 +34,14 @@ export default async function handler(req, res) {
 		)
 		formData.append("model", model)
 
+		const id = setTimeout(
+			() =>
+				res.status(400).send({
+					error:
+						"Whisper is currently experience slowdowns. Please try again later.",
+				}),
+			9000
+		)
 		const response = await fetch(
 			"https://api.openai.com/v1/audio/transcriptions",
 			{
@@ -44,7 +53,7 @@ export default async function handler(req, res) {
 				body: formData,
 			}
 		)
-
+		clearTimeout(id)
 		const { text, error } = await response.json()
 		if (response.ok) {
 			res.status(200).send({ text: text })
