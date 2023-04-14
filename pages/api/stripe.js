ebf87aa22060d5928e63ebd38c22a6ab7f4bbf16
Andrew Bustos <infernious@live.com>
2023-04-14 16:34:07 -0700

    Set up portal redirect

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -17,41 +17,61 @@ export default async function handler(req, res) {
 	const sig = req.headers["stripe-signature"]
 
 	const body = await buffer(req)
-	const event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
 
-	switch (event.type) {
-		case "customer.created":
-			const customerCreated = event.data.object
-			res.status(201).send({ object: customerCreated })
-			break
-		case "customer.subscription.created":
-			const subscriptionCreated = event.data.object
-			res.status(201).send({ object: subscriptionCreated })
+	if (body.text !== undefined) {
+		const { text } = body
 
-			break
-		case "customer.subscription.deleted":
-			const subscriptionDeleted = event.data.object
-			res.status(200).send({ object: subscriptionDeleted })
-			break
-		default:
-			console.log(`Unhandled event type ${event.type}`)
+		if (text === "portal") {
+			const { customerId } = body
+
+			// This is the url to which the customer will be redirected when they are done
+			// managing their billing with the portal.
+			const returnUrl = "https://app.lirical.xyz"
+
+			const portalSession = await stripe.billingPortal.sessions.create({
+				customer: customerId,
+				return_url: returnUrl,
+			})
+
+			res.redirect(303, portalSession.url)
+		}
+	} else {
+		const event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
+
+		switch (event.type) {
+			case "customer.created":
+				const customerCreated = event.data.object
+				res.status(201).send({ object: customerCreated })
+				break
+			case "customer.subscription.created":
+				const subscriptionCreated = event.data.object
+				res.status(201).send({ object: subscriptionCreated })
+
+				break
+			case "customer.subscription.deleted":
+				const subscriptionDeleted = event.data.object
+				res.status(200).send({ object: subscriptionDeleted })
+				break
+			default:
+				console.log(`Unhandled event type ${event.type}`)
+		}
+
+		res.send()
 	}
 
-	res.send()
-}
+	const buffer = (req) => {
+		return new Promise((resolve, reject) => {
+			const chunks = []
 
-const buffer = (req) => {
-	return new Promise((resolve, reject) => {
-		const chunks = []
+			req.on("data", (chunk) => {
+				chunks.push(chunk)
+			})
 
-		req.on("data", (chunk) => {
-			chunks.push(chunk)
+			req.on("end", () => {
+				resolve(Buffer.concat(chunks))
+			})
+
+			req.on("error", reject)
 		})
-
-		req.on("end", () => {
-			resolve(Buffer.concat(chunks))
-		})
-
-		req.on("error", reject)
-	})
+	}
 }
