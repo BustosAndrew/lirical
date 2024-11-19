22012916e9612490be1f93e4181ba148c0ab7627
Andrew Bustos <infernious@live.com>
2024-11-19 06:39:43 -0800

    Update OpenAIStream.js

--- helpers/OpenAIStream.js
+++ helpers/OpenAIStream.js
@@ -12,7 +12,7 @@ export async function OpenAIStream(payload) {
 
 	// const completion = await openai.createChatCompletion(payload)
 
-	const res = await fetch("https://api.openai.com/v1/chat/completions", {
+	const res = await fetch("https://oai.helicone.ai/v1/chat/completions", {
 		headers: {
 			"Content-Type": "application/json",
 			Authorization: `Bearer ${process.env.OPEN_AI_KEY ?? ""}`,
